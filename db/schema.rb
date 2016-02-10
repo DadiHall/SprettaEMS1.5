b688eac8999124445a625a1102a1b76a7116e4bc
DadiHall <dadih32@gmail.com>
2016-02-10 18:16:22 +0000

    Added staf to users, added registrations_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210173236) do
+ActiveRecord::Schema.define(version: 20160210180442) do
 
   create_table "papers", force: :cascade do |t|
     t.string   "paper_type"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160210173236) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "staff"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
