93718a4f7016b6d06968e88e7544a15bc43480f6
DadiHall <dadih32@gmail.com>
2016-02-12 17:41:17 +0000

    Added running total for Paper, trees and Kwst in Applictation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
+
+  before_action :load_running_totals
+
+  def load_running_totals
+    # method 1. Similar to how the calculation is done in the view.
+    # @paper_weight_per_capita = Papers.all.map(&:paper_weight_per_capita, current_user).reduce(&:+)
+
+    # method 2. does the summation in the database without having to load all the records into ruby. (weight needs to be a number in the db.)
+    @paper_weight_per_capita = Paper.sum(:paper_weight) / current_user.staff
+
+    @paper_tree_ratio = Paper.sum(:paper_weight) / 1000 
+    # Rafmagnsreikningar
+    @electro_total_per_capita = Electro.sum(:electricity_kwst) / current_user.staff
+  end
+
 end
