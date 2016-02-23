c96ac8faf09e59d3b2383f94aa09a67e59ef1739
DadiHall <dadih32@gmail.com>
2016-02-23 18:18:36 +0000

    added user_totals in partials, Added formulas for travel in application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,17 @@ class ApplicationController < ActionController::Base
         #Ræsting   
            @cleaning_staff_ratio = current_user.cleanings.sum(:cleaning_liter).to_i / current_user.profile.staff
            @cleaning_building_ratio = current_user.cleanings.sum(:cleaning_liter) / current_user.profile.building_size
-        #Ferðalog
-           #hér koma formúlur f. ferðalög  
+        #Ferðalog-bilar
+           @transport_co2_km = current_user.transports.sum(:transport_km) * (0.1404)
+           @transport_co2_km_staff_ratio = current_user.transports.sum(:transport_km) * (0.1404) / current_user.profile.staff
+           @transport_km_staff_ratio = current_user.transports.sum(:transport_km) / current_user.profile.staff
+           @transport_km = current_user.transports.sum(:transport_km)
+        #Ferðalog-flug
+          @transport_flight_co2 = current_user.transports.sum(:transport_flight_km).to_i * (0.1722)   
+          @transport_flight_co2_staff_ratio = current_user.transports.sum(:transport_flight_km).to_i * (0.1722) / current_user.profile.staff
+          @transport_flight_km = current_user.transports.sum(:transport_flight_km).to_i 
+          @transport_flight_km_staff_ratio = current_user.transports.sum(:transport_flight_km).to_i / current_user.profile.staff
+
     else 
       flash[:notice] = "No profile exists for current user"
       redirect_to new_user_profile_path(current_user) if current_user and return
@@ -36,6 +45,7 @@ class ApplicationController < ActionController::Base
  
 
     @paper_tree_ratio = current_user.papers.sum(:paper_weight) / 1000 
+
     # Rafmagnsreikningar
     #unless current_user.profile.blank?  
       #@electro_total_per_capita = Electro.sum(:electricity_kwst) / current_user.profile.staff 
