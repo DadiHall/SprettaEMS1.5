aac2d2af34dce5d8aa05e522dbaa9c09abd4e3ff
DadiHall <dadih32@gmail.com>
2016-02-29 21:06:21 +0000

    repaired the profile login forn non exixting profiles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,10 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:staff, :name, :email, :password, :password_confirmation, :current_password)
   end
+
+   protected
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_profile_path(current_user) # Or :prefix_to_your_route
+  end
 end
