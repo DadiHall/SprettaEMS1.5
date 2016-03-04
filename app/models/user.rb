574f273b47661ade044626280785efa431864efa
DadiHall <dadih32@gmail.com>
2016-03-04 12:34:16 +0000

    Finally Fixed the bug in Friends show section

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ has_many :garbages
 
 has_many :pages
 
-has_many :friendships
+has_many :friendships, dependent: :destroy
 has_many :friends, through: :friendships
 has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 has_many :inverse_friends, through: :inverse_friendships, :source => :user
